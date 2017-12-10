1a01c69fd4edf7af281788da7b8e41afe052bd34
Behzad Mozaffari <behzad.mozaffari@papercut.com>
2017-12-10 20:11:43 +1100

    add zero test

--- src/main/java/com/behzadmozaffari/shopping/Checkout.java
+++ src/main/java/com/behzadmozaffari/shopping/Checkout.java
@@ -15,6 +15,6 @@ public class Checkout {
     }
 
     public BigDecimal total() {
-        return null;
+        return BigDecimal.ZERO;
     }
 }
--- src/test/java/com/behzadmozaffari/shopping/CheckoutTest.java
+++ src/test/java/com/behzadmozaffari/shopping/CheckoutTest.java
@@ -2,7 +2,11 @@ package com.behzadmozaffari.shopping;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class CheckoutTest {
     @Test
@@ -11,5 +15,12 @@ public class CheckoutTest {
         co.scan(Item.ipd);
         co.total();
     }
+
+    @Test
+    public void shouldReturnZeroWhenNothingScanned() {
+        Checkout co = new Checkout(new ArrayList<>());
+        assertThat(co.total(), is(BigDecimal.ZERO));
+    }
+
 
 }
